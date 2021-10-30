1345e972f096e7d3b2c38f86662f46afbf13260b
truon <truongth2904@gmail.com>
2021-10-30 19:07:42 +0700

    update login

---
+++ app/src/main/java/com/example/warehousemanager/LoginActivity.java
@@ -0,0 +1,71 @@
+package com.example.warehousemanager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText edtEmail, edtPassword;
+    Button btnLogin;
+    FirebaseAuth auth;
+    FirebaseUser currentUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_login);
+        setTitle("Đăng Nhập");
+        auth = FirebaseAuth.getInstance();
+        Mapping();
+        setEvent();
+    }
+
+    private void setEvent() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkLogin();
+            }
+        });
+    }
+
+    private void checkLogin() {
+        String email = edtEmail.getText().toString().trim();
+        String password = edtPassword.getText().toString().trim();
+        if (email.isEmpty()) {
+            edtEmail.setError("Trường email đang trống!");
+        } else if (password.isEmpty()) {
+            edtPassword.setError("Trường mật khẩu đang trống!");
+        } else {
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Đăng nhập không thành công!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void Mapping() {
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+    }
+
+}
