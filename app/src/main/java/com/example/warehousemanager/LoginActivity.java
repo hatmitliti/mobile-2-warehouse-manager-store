ca09cd0aea8babbcb73ac325dadc829ccf8e18a0
truon <truongth2904@gmail.com>
2021-10-30 20:04:51 +0700

    fix depedenci

--- app/src/main/java/com/example/warehousemanager/LoginActivity.java
+++ app/src/main/java/com/example/warehousemanager/LoginActivity.java
@@ -1,8 +1,5 @@
 package com.example.warehousemanager;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,8 +7,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,6 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     Button btnLogin;
     FirebaseAuth auth;
     FirebaseUser currentUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
