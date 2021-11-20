8f474bfe673033a1722ec2680486cb898cc30b0c
Trường Vũ <truongvu6021@gmail.com>
2021-11-20 20:54:18 +0700

    fix loading dialog and some bug

--- app/src/main/java/com/example/warehousemanager/FirstLoad.java
+++ app/src/main/java/com/example/warehousemanager/FirstLoad.java
@@ -17,30 +17,15 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class FirstLoad extends AppCompatActivity {
-//    Animation topAnim, bottomAnim;
-//    ImageView image;
-//    TextView nhom6, slogan;
-//    LinearLayout lnName;
     private static int SPLASH_SCREEN = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-       // getSupportActionBar().hide();
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.first_load);
-
-//        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
-//        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
-//
-//        image = findViewById(R.id.imageView);
-//        nhom6 = findViewById(R.id.nhom6);
-//        lnName = findViewById(R.id.lnName);
-//
-//        image.setAnimation(topAnim);
-//        nhom6.setAnimation(topAnim);
-//        lnName.setAnimation(bottomAnim);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -48,17 +33,18 @@ public class FirstLoad extends AppCompatActivity {
                 checklogin();
                 finish();
             }
-        },SPLASH_SCREEN);
+        }, SPLASH_SCREEN);
 
 
     }
 
     private void checklogin() {
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        if(mAuth != null){
-            startActivity(new Intent(FirstLoad.this,MainActivity.class));
-        }else {
-            startActivity(new Intent(FirstLoad.this,LoginActivity.class));
+        FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (mUser == null) {
+            startActivity(new Intent(FirstLoad.this, LoginActivity.class));
+        } else {
+            startActivity(new Intent(FirstLoad.this, MainActivity.class));
         }
     }
 }
